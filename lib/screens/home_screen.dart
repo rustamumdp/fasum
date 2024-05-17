fce26a44accba37699ba84a773cab6c2f167b5bc
rustamumdp <rustameffendi029@mhs.mdp.ac.id>
2024-05-17 10:54:11 +0700

    post

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:fasum/screens/add_post_screen.dart';
 import 'package:fasum/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -23,8 +25,47 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-        child: const Text('You are logged in'),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('posts').orderBy('timestamp', descending: true).snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final posts = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              final postData = post.data() as Map<String, dynamic>;
+              final imageUrl = postData['image_url'] ?? '';
+              final postText = postData['text'] ?? '';
+              final timestamp = postData['timestamp'] as Timestamp;
+              final formattedTime = DateFormat.yMMMd().add_jm().format(timestamp.toDate());
+
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      imageUrl.isNotEmpty
+                          ? Image.network(imageUrl)
+                          : const Icon(Icons.image_not_supported),
+                      const SizedBox(height: 8.0),
+                      Text('Posted by User', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(formattedTime, style: TextStyle(color: Colors.grey)),
+                      const SizedBox(height: 8.0),
+                      Text(postText),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
